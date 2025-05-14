c02241a8178df3ba83d9eff7b8cc46e48a4cf898
ivr42069 <ivardekker1@gmail.com>
2025-05-14 13:27:39 +0200

    Aanpassingen aan dashboard1

--- sensor_api.php
+++ sensor_api.php
@@ -14,12 +14,15 @@ curl_setopt($loginCurl, CURLOPT_POSTFIELDS, $loginPayload);
 curl_setopt($loginCurl, CURLOPT_HTTPHEADER, [
     'Content-Type: application/json'
 ]);
+// Forceer HTTP/1.1
+curl_setopt($loginCurl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
 
 $loginResponse = curl_exec($loginCurl);
 $loginStatus = curl_getinfo($loginCurl, CURLINFO_HTTP_CODE);
+$curlError = curl_error($loginCurl);
 curl_close($loginCurl);
 
-if ($loginStatus === 200) {
+if ($loginStatus === 200 && $loginResponse) {
     $loginData = json_decode($loginResponse, true);
     $token = $loginData['authToken'];
 
@@ -29,13 +32,20 @@ if ($loginStatus === 200) {
     curl_setopt($dataCurl, CURLOPT_HTTPHEADER, [
         "Authorization: Bearer $token"
     ]);
+    // Forceer HTTP/1.1
+    curl_setopt($dataCurl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
 
     $dataResponse = curl_exec($dataCurl);
+    $dataError = curl_error($dataCurl);
     curl_close($dataCurl);
 
-    echo $dataResponse;
+    echo $dataResponse ?: json_encode(["error" => "Data ophalen mislukt", "curl_error" => $dataError]);
 } else {
-
     http_response_code(401);
-    echo json_encode(["error" => "Login failed"]);
+    echo json_encode([
+        "error" => "Login mislukt",
+        "status" => $loginStatus,
+        "curl_error" => $curlError,
+        "raw_response" => $loginResponse
+    ]);
 }

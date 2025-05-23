762fe1b848848361cf5096d910c1d0150f05c40a
Jerra4 <145442633+Jerra4@users.noreply.github.com>
2025-05-23 02:25:03 +0200

    Dashboard wordt nu automatisch geüpdate zonder handmatige invoer. Oude code is intact gebleven mocht het fout zijn.

--- sensor_dashboard.js
+++ sensor_dashboard.js
@@ -3,16 +3,138 @@ document.addEventListener("DOMContentLoaded", () => {
 
     console.log("Hij laadt");
 
-// Hiermee halen we de API op via php
-    fetch("sensor_api.php")
-        .then(response => response.json())
-        // data is response. Response is voor gemak omgezet naar json
-        .then(data => {
-// console log in de browser (alleen voor debuggen)
+    /*
+    // Hiermee halen we de API op via php
+        fetch("sensor_api.php")
+            .then(response => response.json())
+            // data is response. Response is voor gemak omgezet naar json
+            .then(data => {
+    // console log in de browser (alleen voor debuggen)
+                console.log("Hij laadt");
+                console.log(data)
+                console.log(document.getElementById("hum1"))
+    // Variabelen oproepen
+                let stroArray = null;
+                let isoArray = null;
+                let cellArray = null;
+                let hemArray = null;
+                let binnenTempArray = null;
+                const naam0 = "Sensor0";
+                const naam = "Sensor1";
+                const naam2 = "Sensor2";
+                const naam3 = "Sensor3";
+                const naam4 = "Sensor4";
+
+
+    // De array van achteren naar voren checken voor de juiste naam (met debugging)
+                for (let i = data.length - 1; i >= 0; i--) {
+                    if (data[i].sensor_name === naam) {
+                        stroArray = data[i];
+                        console.log("ik draai");
+                        break;
+                    }
+
+                }
+
+                for (let i = data.length - 1; i >= 0; i--) {
+                    if (data[i].sensor_name === naam2) {
+                        isoArray = data[i];
+                        console.log("ik draai");
+                        break;
+                    }
+
+                }
+
+                for (let i = data.length - 1; i >= 0; i--) {
+                    if (data[i].sensor_name === naam3) {
+                        cellArray = data[i];
+                        console.log("ik draai");
+                        break;
+                    }
+
+                }
+
+                for (let i = data.length - 1; i >= 0; i--) {
+                    if (data[i].sensor_name === naam4) {
+                        hemArray = data[i];
+                        console.log("ik draai");
+                        break;
+                    }
+
+
+                }
+
+
+                for (let i = data.length - 1; i >= 0; i--) {
+                    if (data[i].sensor_name === naam0) {
+                        binnenTempArray = data[i];
+                        console.log("ik draai");
+                        break;
+                    }
+
+
+                }
+
+
+    // Extra controle
+                if (stroArray && stroArray.sensor_name === naam) {
+                    document.getElementById("hum1").textContent = stroArray.humidity_1 + " %";
+                    document.getElementById("hum2").textContent = stroArray.humidity_2 + " %";
+                    document.getElementById("hum3").textContent = stroArray.humidity_3 + " %";
+                    document.getElementById("temp1").textContent = stroArray.temperature_1 + "°C";
+                    document.getElementById("temp2").textContent = stroArray.temperature_2 + "°C";
+                    document.getElementById("temp3").textContent = stroArray.temperature_3 + "°C";
+                    document.getElementById("binnen").textContent = binnenTempArray.temperature_1 + "°C";
+                }
+
+                if (isoArray && isoArray.sensor_name === naam2){
+                    document.getElementById("hum1_isovlas").textContent = isoArray.humidity_1 + " %";
+                    document.getElementById("hum2_isovlas").textContent = isoArray.humidity_2 + " %";
+                    document.getElementById("hum3_isovlas").textContent = isoArray.humidity_3 + " %";
+                    document.getElementById("temp1_isovlas").textContent = isoArray.temperature_1 + "°C";
+                    document.getElementById("temp2_isovlas").textContent = isoArray.temperature_2 + "°C";
+                    document.getElementById("temp3_isovlas").textContent = isoArray.temperature_3 + "°C";
+                    document.getElementById("binnen_isovlas").textContent = binnenTempArray.temperature_1 + "°C";
+
+                }
+
+                if (cellArray && cellArray.sensor_name === naam3){
+                    document.getElementById("hum1_cellulose").textContent = cellArray.humidity_1 + " %";
+                    document.getElementById("hum2_cellulose").textContent = cellArray.humidity_2 + " %";
+                    document.getElementById("hum3_cellulose").textContent = cellArray.humidity_3 + " %";
+                    document.getElementById("temp1_cellulose").textContent = cellArray.temperature_1 + "°C";
+                    document.getElementById("temp2_cellulose").textContent = cellArray.temperature_2 + "°C";
+                    document.getElementById("temp3_cellulose").textContent = cellArray.temperature_3 + "°C";
+                    document.getElementById("binnen_cellulose").textContent = binnenTempArray.temperature_1 + "°C";
+                }
+
+                if (hemArray && hemArray.sensor_name === naam4){
+                    document.getElementById("hum1_hemkor").textContent = hemArray.humidity_1 + " %";
+                    document.getElementById("hum2_hemkor").textContent = hemArray.humidity_2 + " %";
+                    document.getElementById("hum3_hemkor").textContent = hemArray.humidity_3 + " %";
+                    document.getElementById("temp1_hemkor").textContent = hemArray.temperature_1 + "°C";
+                    document.getElementById("temp2_hemkor").textContent = hemArray.temperature_2 + "°C";
+                    document.getElementById("temp3_hemkor").textContent = hemArray.temperature_3 + "°C";
+                    document.getElementById("binnen_hemkor").textContent = binnenTempArray.temperature_1 + "°C";
+                }
+
+
+            })
+
+            .catch(error => {
+                console.error("Fout bij het ophalen"+ error);
+
+            });
+    })
+    */
+    async function fetchSensorData() {
+        try {
+            const response = await fetch("sensor_api.php");
+            const data = await response.json();
             console.log("Hij laadt");
             console.log(data)
             console.log(document.getElementById("hum1"))
-// Variabelen oproepen
+            // Variabelen oproepen
             let stroArray = null;
             let isoArray = null;
             let cellArray = null;
@@ -25,7 +147,7 @@ document.addEventListener("DOMContentLoaded", () => {
             const naam4 = "Sensor4";
 
 
-// De array van achteren naar voren checken voor de juiste naam (met debugging)
+            // De array van achteren naar voren checken voor de juiste naam (met debugging)
             for (let i = data.length - 1; i >= 0; i--) {
                 if (data[i].sensor_name === naam) {
                     stroArray = data[i];
@@ -75,7 +197,7 @@ document.addEventListener("DOMContentLoaded", () => {
             }
 
 
-// Extra controle
+            // Extra controle
             if (stroArray && stroArray.sensor_name === naam) {
                 document.getElementById("hum1").textContent = stroArray.humidity_1 + " %";
                 document.getElementById("hum2").textContent = stroArray.humidity_2 + " %";
@@ -86,7 +208,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 document.getElementById("binnen").textContent = binnenTempArray.temperature_1 + "°C";
             }
 
-            if (isoArray && isoArray.sensor_name === naam2){
+            if (isoArray && isoArray.sensor_name === naam2) {
                 document.getElementById("hum1_isovlas").textContent = isoArray.humidity_1 + " %";
                 document.getElementById("hum2_isovlas").textContent = isoArray.humidity_2 + " %";
                 document.getElementById("hum3_isovlas").textContent = isoArray.humidity_3 + " %";
@@ -97,7 +219,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
             }
 
-            if (cellArray && cellArray.sensor_name === naam3){
+            if (cellArray && cellArray.sensor_name === naam3) {
                 document.getElementById("hum1_cellulose").textContent = cellArray.humidity_1 + " %";
                 document.getElementById("hum2_cellulose").textContent = cellArray.humidity_2 + " %";
                 document.getElementById("hum3_cellulose").textContent = cellArray.humidity_3 + " %";
@@ -107,7 +229,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 document.getElementById("binnen_cellulose").textContent = binnenTempArray.temperature_1 + "°C";
             }
 
-            if (hemArray && hemArray.sensor_name === naam4){
+            if (hemArray && hemArray.sensor_name === naam4) {
                 document.getElementById("hum1_hemkor").textContent = hemArray.humidity_1 + " %";
                 document.getElementById("hum2_hemkor").textContent = hemArray.humidity_2 + " %";
                 document.getElementById("hum3_hemkor").textContent = hemArray.humidity_3 + " %";
@@ -116,13 +238,10 @@ document.addEventListener("DOMContentLoaded", () => {
                 document.getElementById("temp3_hemkor").textContent = hemArray.temperature_3 + "°C";
                 document.getElementById("binnen_hemkor").textContent = binnenTempArray.temperature_1 + "°C";
             }
-
-
-        })
-
-        .catch(error => {
-            console.error("Fout bij het ophalen"+ error);
-
-        });
+        } catch (error) {
+            console.error('Fout bij ophalen sensor data:', error);
+        }
+    }
+    fetchSensorData();
+    setInterval(fetchSensorData, 600000);
 })
-
--- weer_api.js
+++ weer_api.js
@@ -1,5 +1,5 @@
 weer_API = ("https://weerlive.nl/api/weerlive_api_v2.php?key=cd356cbcde&locatie=52.3532452,5.1483313")
-
+/*
 fetch(weer_API)
     .then(r => r.json())
     .then(x => {
@@ -14,3 +14,27 @@ fetch(weer_API)
             document.getElementById("buiten_cellulose").textContent = huidige_weer.temp
             document.getElementById("buiten_hemkor").textContent = huidige_weer.temp
     })
+*/
+
+async function fetchWeerApi() {
+    try {
+        const res = await fetch(weer_API)
+        const data = await res.json()
+        let huidige_weer;
+        huidige_weer = data.liveweer[0]
+        console.log(huidige_weer.temp)
+        const now = new Date()
+        console.log("Het is nu: " + now.toLocaleTimeString());
+
+        document.getElementById("buiten").textContent = huidige_weer.temp + "°C";
+        document.getElementById("buiten_isovlas").textContent = huidige_weer.temp
+        document.getElementById("buiten_cellulose").textContent = huidige_weer.temp
+        document.getElementById("buiten_hemkor").textContent = huidige_weer.temp
+    } catch (error) {
+        console.log("Error " + error)
+    }
+}
+
+fetchWeerApi();
+setInterval(fetchWeerApi, 600000);
+
